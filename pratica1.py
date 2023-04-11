8c9219ce9294cc4f7afe88e0219cc45882c752e9
RafaelCardosoBraganca <rafacardoso635@gmail.com>
2023-04-11 18:36:25 -0300

    Create pratica1.py

---
+++ pratica1.py
@@ -0,0 +1,10 @@
+
+cores = {'limpa':'\033[m',
+         'azul':'\033[34m',
+         'amarelo':'\033[33m',
+         'pretoebranco':'\033[m'}
+
+print('{}Olá, mundo!!{} aqui já não tem cores'.format(cores['azul'], cores['limpa']))
+
+s= 'prova de python'
+print(len(s))
